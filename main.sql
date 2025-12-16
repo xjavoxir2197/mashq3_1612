e58f0817a471ddccb2c536e0732310ea96fae692
xjavoxir2197 <xjavoxir2197@gmail.com>
2025-12-16 15:42:50 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,17 @@
+-- 11-misol
+INSERT INTO Talabalar (id, ism, yosh, GPA) VALUES
+(1, 'Ali', 20, 3.5),
+(2, 'Vali', 21, 3.8),
+(3, 'Hasan', 19, 3.2);
+
+-- 12-misol
+SELECT * FROM Mahsulotlar;
+
+-- 13-misol
+SELECT nom, narx FROM Mahsulotlar;
+
+-- 14-misol
+SELECT GPA FROM Talabalar;
+
+-- 15-misol
+SELECT * FROM Xodimlar;
